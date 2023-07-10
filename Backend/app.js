532b74da55d80218954f39bc28a728f6dad43ddc
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2023-07-10 23:37:03 +0300

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,15 @@ import Express from "express";
 import {app1} from "./api/user.routes.js";
 import { DBconnection } from "./config/dbConnection.js";
 import cors from 'cors'
+import  dotenv  from 'dotenv'
+dotenv.config()
 const app = Express()
+const port = process.env.PORT||3000
+
 app.use(Express.json())
 app.use(cors())
 app.use(app1)
 DBconnection()
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is runnnnning");
 })
